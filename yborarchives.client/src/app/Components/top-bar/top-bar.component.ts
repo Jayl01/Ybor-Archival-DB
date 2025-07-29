2f685517dfe64b0d5854e0801198bef6d219d3d6
kusf <130384593+kusf@users.noreply.github.com>
2025-07-28 22:43:52 -0400

    Added top bar to page

---
+++ yborarchives.client/src/app/Components/top-bar/top-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'top-bar',
+  standalone: true,
+  templateUrl: './top-bar.component.html',
+  styleUrl: './top-bar.component.css'
+})
+export class TopBarComponent {
+
+}
